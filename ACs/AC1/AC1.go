4212a602b1b9c6c6f542e1b37520bf88c33975c8
Luigi Fernando <luigifcard3@gmail.com>
2024-02-24 14:05:39 -0300

    ac1 v2

--- ACs/AC1/AC1.go
+++ ACs/AC1/AC1.go
@@ -3,25 +3,36 @@ package main
 import "fmt"
 
 //Fazer
-func calcularMedia (a,b float64){
+func calcularMedia(a, b float64) {
 
 }
 
-func verificarParidade (p int){
+func verificarParidade(p int) string {
+	if p%2 == 0 {
+		return "Par"
+	} else {
+		return "Impar"
+	}
+}
+func minhaPotencia(y, z int) int {
+	res := 1
+	for i := 0; i < z; i++ {
+		res *= y
+	}
+	return res
+}
 
-}
-func minhaPotencia (y,z int)int{
-	return y * y
-}
 //Pronto
 func converteCelsiusParaFahrenheit(x float64) float64 {
-return x*1.8 + 32
+	return x*1.8 + 32
 }
 func main() {
-	y := 3
-	z :=2
+	y := 8
+	z := 3
 	x := 32.0
+	p := 99
 
 	fmt.Println(converteCelsiusParaFahrenheit(x), "°F")
-	fmt.Println(minhaPotencia(y,z))
+	fmt.Println(minhaPotencia(y, z))
+	fmt.Println(verificarParidade(p))
 }
